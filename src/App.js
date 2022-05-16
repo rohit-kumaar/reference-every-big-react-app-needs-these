9b9250252e036612ba88dc6f72ba2e507b4ff2ff
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-05-16 11:58:24 +0530

    useState : HookCounterTwo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import ToggleComponent from "./1-useToggle/ToggleComponent"
-import TimeoutComponent from "./2-useTimeout/TimeoutComponent"
-import DebounceComponent from "./3-useDebounce/DebounceComponent"
-import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent"
-import ArrayComponent from "./5-useArray/ArrayComponent"
-import PreviousComponent from "./6-usePrevious/PreviousComponent"
-import StateWithHistoryComponent from "./7-useStateWithHistory/StateWithHistoryComponent"
-import StorageComponent from "./8-useStorage/StorageComponent"
-import AsyncComponent from "./9-useAsync/AsyncComponent"
-import FetchComponent from "./10-useFetch/FetchComponent"
-import ScriptComponent from "./11-useScript/ScriptComponent"
-import DeepCompareEffectComponent from "./12-useDeepCompareEffect/DeepCompareEffectComponent"
-import EventListenerComponent from "./13-useEventListener/EventListenerComponent"
-import OnScreenComponentComponent from "./14-useOnScreen/OnScreenComponent"
-import WindowSizeComponent from "./15-useWindowSize/WindowSizeComponent"
-import MediaQueryComponent from "./16-useMediaQuery/MediaQueryComponent"
-import GeolocationComponent from "./17-useGeolocation/GeolocationComponent"
-import StateWithValidationComponent from "./18-useStateWithValidation/StateWithValidationComponent"
-import SizeComponent from "./19-useSize/SizeComponent"
-import EffectOnceComponent from "./20-useEffectOnce/EffectOnceComponent"
-import ClickOutsideComponent from "./21-useClickOutside/ClickOutsideComponent"
-import DarkModeComponent from "./22-useDarkMode/DarkModeComponent"
-import CopyToClipboardComponent from "./23-useCopyToClipboard/CopyToClipboardComponent"
-import CookieComponent from "./24-useCookie/CookieComponent"
-import TranslationComponent from "./25-useTranslation/TranslationComponent"
-import OnlineStatusComponent from "./26-useOnlineStatus/OnlineStatusComponent"
-import RenderCountComponent from "./27-useRenderCount/RenderCountComponent"
-import DebugInformationComponent from "./28-useDebugInformation/DebugInformationComponent"
-import HoverComponent from "./29-useHover/HoverComponent"
-import LongPressComponent from "./30-useLongPress.js/LongPressComponent"
+import ToggleComponent from "./1-useToggle/ToggleComponent";
+import TimeoutComponent from "./2-useTimeout/TimeoutComponent";
+import DebounceComponent from "./3-useDebounce/DebounceComponent";
+import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent";
+import ArrayComponent from "./5-useArray/ArrayComponent";
+import PreviousComponent from "./6-usePrevious/PreviousComponent";
+import StateWithHistoryComponent from "./7-useStateWithHistory/StateWithHistoryComponent";
+import StorageComponent from "./8-useStorage/StorageComponent";
+import AsyncComponent from "./9-useAsync/AsyncComponent";
+import FetchComponent from "./10-useFetch/FetchComponent";
+import ScriptComponent from "./11-useScript/ScriptComponent";
+import DeepCompareEffectComponent from "./12-useDeepCompareEffect/DeepCompareEffectComponent";
+import EventListenerComponent from "./13-useEventListener/EventListenerComponent";
+import OnScreenComponentComponent from "./14-useOnScreen/OnScreenComponent";
+import WindowSizeComponent from "./15-useWindowSize/WindowSizeComponent";
+import MediaQueryComponent from "./16-useMediaQuery/MediaQueryComponent";
+import GeolocationComponent from "./17-useGeolocation/GeolocationComponent";
+import StateWithValidationComponent from "./18-useStateWithValidation/StateWithValidationComponent";
+import SizeComponent from "./19-useSize/SizeComponent";
+import EffectOnceComponent from "./20-useEffectOnce/EffectOnceComponent";
+import ClickOutsideComponent from "./21-useClickOutside/ClickOutsideComponent";
+import DarkModeComponent from "./22-useDarkMode/DarkModeComponent";
+import CopyToClipboardComponent from "./23-useCopyToClipboard/CopyToClipboardComponent";
+import CookieComponent from "./24-useCookie/CookieComponent";
+import TranslationComponent from "./25-useTranslation/TranslationComponent";
+import OnlineStatusComponent from "./26-useOnlineStatus/OnlineStatusComponent";
+import RenderCountComponent from "./27-useRenderCount/RenderCountComponent";
+import DebugInformationComponent from "./28-useDebugInformation/DebugInformationComponent";
+import HoverComponent from "./29-useHover/HoverComponent";
+import LongPressComponent from "./30-useLongPress.js/LongPressComponent";
+import HookCounter from "./31-useState/HookCounter";
+import HookCounterTwo from "./31-useState/HookCounterTwo";
 
 function App() {
   // return <ToggleComponent />
@@ -50,7 +52,7 @@ function App() {
   // return <StateWithValidationComponent />
   // return <SizeComponent />
   // return <EffectOnceComponent />
-  return <ClickOutsideComponent />
+  // return <ClickOutsideComponent />;
   // return <DarkModeComponent />
   // return <CopyToClipboardComponent />
   // return <CookieComponent />
@@ -60,6 +62,8 @@ function App() {
   // return <DebugInformationComponent />
   // return <HoverComponent />
   // return <LongPressComponent />
+  // return <HookCounter />;
+  return <HookCounterTwo />;
 }
 
-export default App
+export default App;
