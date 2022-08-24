e9233a90f1589767d524c8a998b6ad33bdc86bc5
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-08-24 17:42:12 +0530

    update : algo

--- src/31-useState/AllInputsWithAsingleonChange.js
+++ src/31-useState/AllInputsWithAsingleonChange.js
@@ -2,34 +2,79 @@ import React, { useState } from "react";
 
 function AllInputsWithAsingleonChange() {
   const [user, setUser] = useState({
-    firstName:'',
-    lastName:'',
-    userName:'',
-    email:'',
-    password:'',
-    country:'',
-    city:'',
-    address:'',
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+    country: "",
+    city: "",
+    address: "",
   });
 
   const handleChange = (e) => {
-    setUser((prev) => ({...prev, [e.target.name]:e.target.value}))
-  }
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
 
-  console.log(user);
+  const handleSubmit = () => {
+    console.log(user);
+  };
 
   return (
     <div>
       <form>
-        <input type="text" name="firstName" placeholder="First name" onChange={handleChange} /> <br />
-        <input type="text" name="lastName" placeholder="Last name" onChange={handleChange} /> <br />
-        <input type="text" name="userName" placeholder="User name" onChange={handleChange} /> <br />
-        <input type="email" name="email" placeholder="Email " onChange={handleChange} /> <br />
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} /> <br />
-        <input type="text" name="country" placeholder="Country" onChange={handleChange} /> <br />
-        <input type="text" name="city" placeholder="City" onChange={handleChange} /> <br />
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First name"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last name"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="userName"
+          placeholder="User name"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email "
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="country"
+          placeholder="Country"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="city"
+          placeholder="City"
+          onChange={handleChange}
+        />
+        <br />
         <input type="text" name="address" placeholder="Address" /> <br />
-        <input type="submit" value="Submit" onChange={handleChange} />
+        <input type="submit" value="Submit" onClick={handleSubmit} />
       </form>
     </div>
   );
